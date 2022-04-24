44680717a58589acb9ad99085e9d7675d8360aeb
ppunikim <yd62322@naver.com>
2022-04-24 21:20:52 +0900

    4월 24일과제

---
+++ Java_Result/src/com/callor/result/JavaResult.java
@@ -0,0 +1,13 @@
+package com.callor.result;
+
+public class JavaResult {
+	public static void main(String[] args) {
+		int intNum = 0;
+		intNum = 10;
+		
+		System.out.print("intNum에 저장된 값은 : " + intNum + "\n");
+		System.out.printf("intNum에 저장된 값은 : %d \n",intNum);
+		System.out.println("intNum에 저장된 값은 : " + intNum);
+	}
+
+}
