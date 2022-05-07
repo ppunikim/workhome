3b6f8205a03a23ac19a6087626b59e9b6eafaf67
ppunikim <yd62322@naver.com>
2022-05-07 23:28:16 +0900

    App 파일 01번 하는 중

---
+++ Java_50_App_01/src/com/callor/app/exec/ListEx2.java
@@ -0,0 +1,26 @@
+package com.callor.app.exec;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class ListEx2 {
+	public static void main(String[] args) {
+		
+		List<Integer> intList = new ArrayList<Integer>();
+		for( int i = 0; i < 55; i ++ ) {
+			intList.add(i + 1);
+		}
+		System.out.println(intList);
+		
+		Collections.shuffle(intList);
+		System.out.println(intList);
+		
+		for(int i = 0; i < 7; i ++) {
+			System.out.printf("%d, ", intList.get(i));
+		}
+		
+		
+		
+	}//end main
+}//end class
